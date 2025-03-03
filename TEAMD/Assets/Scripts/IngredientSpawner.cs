0d5e41ed219ca5607436e7af7c8372e714364730
Mechanizatio <c3lingling@gmail.com>
2025-03-03 18:32:18 +0800

    Integrated JSON
    -Added a DataManager, ingredient.json and recipe.json in StreamingAssets to load data
    -Recoded how ingredients spawn using JSON data in ingredient.json and IngredientSpawner.cs
    -Recoded how order is generated in OrderManager.cs
    -Spawn customer at specific spawn locations and reduced spawnrate in CustomerSpawner.cs
    -Adjusted spawnrate of ingredients to spawn faster based on timer
    -Adjusted the order bubble placements and prefab sizes

--- TEAMD/Assets/Scripts/IngredientSpawner.cs
+++ TEAMD/Assets/Scripts/IngredientSpawner.cs
@@ -4,21 +4,54 @@ using UnityEngine;
 
 public class IngredientSpawner : MonoBehaviour
 {
-    public List<GameObject> ingredients;
+    public static IngredientSpawner instance;
     public List<Vector3> spawnPoints;
     public float spawnrate = 0.8f; //based off timer in other script, apply multiplier
 
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
         InvokeRepeating(nameof(SpawnIngredient), 3f, spawnrate);
     }
 
+    void Update()
+    {
+        //adjust spawnrate based on time in level in GameManager.cs
+        if(GameManager.instance.levelTime <= 120 && GameManager.instance.levelTime > 60)
+        {
+            spawnrate =- 0.3f;
+        }
+        else if(GameManager.instance.levelTime <= 60)
+        {
+            spawnrate =- 0.2f;
+        }
+    }
+
     void SpawnIngredient()
     {
-        GameObject ingredientToSpawn = ingredients[Random.Range(0, ingredients.Count)];
+        //randomize a key from the ingredient dictionary
+        List<string> ingredientKeys = new List<string>(DataManager.instance.ingredientData.Keys);
+        string randomIngredientKey = ingredientKeys[Random.Range(0, ingredientKeys.Count)];
+
+        //select an ingredient using randomized key
+        Ingredient ingredientToSpawn = DataManager.instance.ingredientData[randomIngredientKey];
         Vector3 spawnPosition = spawnPoints[Random.Range(0, spawnPoints.Count)];
 
-        Instantiate(ingredientToSpawn, spawnPosition, Quaternion.identity);
+        //spawn prefab with same name
+        if(DataManager.instance.ingredientPrefabs.TryGetValue(ingredientToSpawn.name, out GameObject ingredientPrefab))
+        {
+            Instantiate(ingredientPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 }
