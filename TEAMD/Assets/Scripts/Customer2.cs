3648d4b3f84cf5cdafef4dd6a50cad22a7bf0ccd
Nekorene <serenethurn@gmail.com>
2025-03-26 21:15:32 +0800

    changed customer scripts
    HAVENT FOUND ANY INGREDIENTS SPRITES T^T, deleted old sprites

--- TEAMD/Assets/Scripts/Customer2.cs
+++ TEAMD/Assets/Scripts/Customer2.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Customer2 : Customer
 {
     private float orderChangeTime;
+    private bool hasChangedOrder = false;
 
     protected override void Start()
     {
@@ -16,14 +17,27 @@ public class Customer2 : Customer
     protected override void Update()
     {
         base.Update();
-        if (patienceMeter <= orderChangeTime)
+
+         // If patience reaches half and the order hasn't changed yet, change it
+        if (!hasChangedOrder && patienceMeter <= orderChangeTime)
         {
             ChangeOrder();
+            hasChangedOrder = true;
         }
     }
 
     private void ChangeOrder()
     {
-       // orderArray = OrderManager.GenerateOrder(); // Generate a new order
+       // Destroy current order visually
+        OrderManager.instance.DestroyOrder(spawnPoint);
+
+        // Generate a new order
+        List<GameObject> newOrder = OrderManager.instance.GenerateOrder(spawnPoint);
+        orderArray = newOrder.ToArray();
+
+        // Display the new order visually
+        OrderManager.instance.DisplayOrder(spawnPoint, newOrder);
+
+        Debug.Log("Customer changed order!");
     }
 }
